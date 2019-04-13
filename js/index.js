0ffd007b3f6fb19b473f0bc7be820a349fc058b5
Ernesto Pena <ernestopenad@gmail.com>
2019-04-12 22:15:21 -0500

    Two events completed

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+const h2_events = document.querySelectorAll("h2");
+
+h2_events.forEach(element => {
+        element.addEventListener('mouseover', function(e) {
+        e.target.style.color = 'blue';
+        });
+        element.addEventListener('mouseleave', function(e){
+            e.target.style.color = 'red';
+        });
+})
+
+
+const menu_doubleclick = document.querySelector('.nav-container')
+const na_anchors = document.querySelectorAll('.nav-link');
+console.log(menu_doubleclick)
+
+    menu_doubleclick.addEventListener('dblclick', function(e){
+        na_anchors.forEach(function(element){
+            element.classList.toggle('crazy_menu');
+        })    
+    })
